e5055254d71082d7cc96d11bdd6994a6529dfcae
”Daniil <lemk@ukr.net>
2017-08-18 21:44:52 +0300

    add application catalog

--- music/settings.py
+++ music/settings.py
@@ -77,8 +77,11 @@ WSGI_APPLICATION = 'music.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, '..', 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.mysql',
+        'HOST': 'localhost',
+        'USER': 'music_db_user',
+        'PASSWORD': 'music1',
+        'NAME': 'music_db',
     }
 }
 
