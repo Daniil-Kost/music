e174363c3f391115d527047315f46879e74dc7ef
”Daniil <lemk@ukr.net>
2017-08-22 11:08:58 +0300

    Deployment commit

--- music/settings.py
+++ music/settings.py
@@ -79,20 +79,13 @@ WSGI_APPLICATION = 'music.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/1.9/ref/settings/#databases
 
-#DATABASES = {
-#    'default': {
-#        'ENGINE': 'django.db.backends.mysql',
-#        'HOST': 'localhost',
-#        'USER': 'music_db_user',
-#        'PASSWORD': 'music1',
-#        'NAME': 'music_db',
-#    }
-#}
-
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, '..', 'db.sqlite3'),
+       	'ENGINE': 'django.db.backends.mysql',
+        'HOST': 'localhost',
+        'USER': 'music_db_user',
+        'PASSWORD': 'music1',
+        'NAME': 'music_db',
     }
 }
 
